33133291e976691fd36bfddd735cab8328ff0fca
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-05-23 21:31:48 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  let [counter, changeCounter] = useState(1);
+  window.changeCounter = changeCounter;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and try to save to reload.
+        </p>
+        <h1> {counter}</h1>
+        <h4> {counter}</h4>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
